ad72b7a4e558ea5970236b5fccb287136c65d439
Rodrigo Sanchez <sanchezgaos@protonmail.com>
2020-04-03 19:46:37 -0600

    initial draft for cassandra schema

---
+++ db/cassandra.sql
@@ -0,0 +1,25 @@
+
+CREATE KEYSPACE soundCloud WITH replication = {‘class’: ‘SimpleStrategy’, ‘replication_factor’ : 3};
+
+CREATE TYPE comment.user (
+  name text, 
+  location text, 
+  followers counter, 
+  pic_url, text 
+);
+
+CREATE TYPE comment.reply (
+  created_at timestamp,
+  comment text,
+  user <user> 
+);
+
+CREATE TABLE comment.comments_by_song (
+  song_id counter, 
+  comment_id counter,
+  created_at timestamp,
+  comment text,
+  user <user>, 
+  replies list <reply>,
+  PRIMARY KEY (song_id)
+);
