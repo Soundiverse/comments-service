f3b6da29168e6edf06b5d3848732051dfe97d351
Rodrigo Sanchez <sanchezgaos@protonmail.com>
2020-04-07 14:15:54 -0500

    Add final version of postgres schema and datagen.sh

--- db/datagen1.sh
+++ db/datagen1.sh
@@ -4,31 +4,30 @@ printf ">\n>>>>> Writing users.csv with 1.5m entries..."
 node --max-old-space-size=10000 /Users/rodrigosanchezgaos/repos/sdc/comments-profile/db/ps-seed-users.js
 printf ">\n>>>>> Writing songs.csv with 10m entries..."
 node --max-old-space-size=10000 /Users/rodrigosanchezgaos/repos/sdc/comments-profile/db/ps-seed-songs.js
-printf ">\nWriting comments1.csv with first 14m entries..."
+printf ">\n>>>>> Writing comments1.csv with first 14m entries..."
 node --max-old-space-size=10000 /Users/rodrigosanchezgaos/repos/sdc/comments-profile/db/ps-seed-comments1.js
-printf ">\n>>>>> Writing comments1.csv with next 14m entries..."
+printf ">\n>>>>> Writing comments2.csv with next 14m entries..."
 node --max-old-space-size=10000 /Users/rodrigosanchezgaos/repos/sdc/comments-profile/db/ps-seed-comments2.js
-printf ">\n>>>>>> Writing comments1.csv with next 14m entries..."
+printf ">\n>>>>>> Writing comments3.csv with next 14m entries..."
 node --max-old-space-size=10000 /Users/rodrigosanchezgaos/repos/sdc/comments-profile/db/ps-seed-comments3.js
-printf ">\n>>>>>> Writing comments1.csv with final 14m entries..."
+printf ">\n>>>>>> Writing comments4.csv with final 14m entries..."
 node --max-old-space-size=10000 /Users/rodrigosanchezgaos/repos/sdc/comments-profile/db/ps-seed-comments4.js
-printf ">\n>>>>>> CSV files for PostgreSQL written: 1.5M users, 10M Songs and 56M comments!!!"
+printf ">\n>>>>>> CSV files for PostgreSQL written: 1.5M users, 10M Songs and 28M comments!!!\n"
 
-printf ">\n>>>>>> Moving csv files to 'data' folder"
+printf ">\n>>>>>> Moving csv files to 'data' folder\n"
 mv *.csv data
 
-printf ">\n>>>>>> Creating new psql db"
+printf ">\n>>>>>> Creating new psql db\n"
 createdb soundiverse
-printf ">\n>>>>>> Creating schema for db"
+printf ">\n>>>>>> Creating schema for db\n"
 psql -d soundiverse < schema.up.sql
 
-
-printf ">\n>>>>>> Seeding Users and Songs into DB..."
+printf ">\n>>>>>> Seeding Users and Songs into DB...\n"
 psql -d soundiverse < usersANDsongs.sql
-printf ">\n>>>>>> Seeding comments 1-2 into DB..."
+printf ">\n>>>>>> Seeding comments 1-2 into DB...\n"
 psql -d soundiverse < comments1-2.sql
-# printf ">\n>>>>>> Seeding comments 3-4 into DB..."
-# psql -d soundiverse < comments1-4.sql
+printf ">\n>>>>>> Seeding comments 3-4 into DB...\n"
+psql -d soundiverse < comments3-4.sql
 printf ">\n>>>>>> All done! WOOOO!!! :) "
 
 
