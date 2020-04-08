762f0f5e44309e0c940ad9953b8fbad551b29962
Rodrigo Sanchez <sanchezgaos@protonmail.com>
2020-04-08 17:28:34 -0500

    Working cassandra driver

--- db/cassandra/cassandra.js
+++ db/cassandra/cassandra.js
@@ -2,18 +2,25 @@ const express = require('express');
 const cassandra = require('cassandra-driver');
 
 const client = new cassandra.Client({
-  contactPoints: ['h1', 'h2'],
+  contactPoints: ['localhost'],
   localDataCenter: 'datacenter1',
   keyspace: 'soundiverse'
 });
 
-const query = 'SELECT * FROM users WHERE song_id = ?';
- 
-client.execute(query, ['8766524'])
-  .then((result) => console.log('cooments are: ', result.rows))
-  .catch((e) => console.log(e));
+client.connect(() => {
+  console.log('connected to cassandra!');
+});
+
 
 const app = express();
 app.listen(3000, () => {
   console.log('Listening on port 3000');
 });
+
+app.get('/comments', (req,res) => {
+  const query = 'SELECT * FROM comments_by_song WHERE song_id = 766524';
+ // [766524], { prepared: true }
+  client.execute(query)
+    .then((result) => console.log('cooments are: ', result.rows))
+    .catch((e) => console.log(e));
+})
