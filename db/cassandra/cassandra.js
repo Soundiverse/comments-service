770a1562687790a27ccb9f237b6c409ac70fc855
Rodrigo Sanchez <sanchezgaos@protonmail.com>
2020-04-08 13:16:35 -0500

    Add draft for cassandra driver

--- db/cassandra/cassandra.js
+++ db/cassandra/cassandra.js
@@ -1,7 +1,18 @@
+const express = require('express');
 const cassandra = require('cassandra-driver');
 
 const client = new cassandra.Client({
   contactPoints: ['h1', 'h2'],
-  localDataCenter: 'datacenter1',
-  keyspace: 'ks1'
+  keyspace: 'soundiverse'
+});
+
+const query = 'SELECT * FROM users WHERE song_id = ?';
+ 
+client.execute(query, ['8766524'])
+  .then((result) => console.log('cooments are: ', result.rows))
+  .catch((e) => console.log(e));
+
+const app = express();
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
 });
--- db/postgres/postgresql.js
+++ db/postgres/postgresql.js
@@ -19,8 +19,34 @@ const app = express();
 
 app.get('/api/songs/:songid/comments', (req, res) => {
   const sonngID = req.params.songid;
-  client.query(`SELECT * FROM comments WHERE song_id = ${sonngID}`)
+  client.query(
+    `select * from public.comments INNER JOIN public.users 
+    ON public.comments."user_id" = public.users.id 
+    where public.comments.song_id = ${sonngID}`)
+
     .then((response) => res.send(response.rows))
+    .catch((e) => console.log(e));
+});
+
+app.post('/api/songs/:songid/comments', (req, res) => {
+  const object = req.body;
+  const demo = `1000001, 400025, 1000001.000, 1584337440, 'First post Request comment!'`;
+  client.query(`INSERT INTO comments (song_id, user_id, float_id, created_at, comment) VALUES (${demo})`)
+    .then((response) => {
+      console.log(Object.keys(response));
+      res.send('Created!');
+    })
+    .catch((e) => console.log(e));
+});
+
+app.delete('/api/songs/:songid/comments/:commentid', (req, res) => {
+  const object = req.body;
+  const demo = req.params.commentid;
+  client.query(`DELETE FROM comments WHERE id = ${demo}`)
+    .then((response) => {
+      console.log(Object.keys(response));
+      res.send('Deleted');
+    })
     .catch((e) => console.log(e));
 });
 
