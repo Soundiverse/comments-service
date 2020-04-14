cc9c57618a0e4a7b9f1ee1b79626e72d8ddd5c3a
Rodrigo Sanchez <sanchezgaos@protonmail.com>
2020-04-13 19:12:57 -0500

    Modify pathways for db to read csv files

--- db/cassandra/seedCassandra3-4.sql
+++ db/cassandra/seedCassandra3-4.sql
@@ -1,4 +1,4 @@
 USE soundiverse;
 
-COPY comments_by_song (id, song_id, comment_ts, created_ts, comment, user_name, location, followers, pic_url) FROM '/Users/rodrigosanchezgaos/repos/sdc/comments-profile/db/cassandra/comments3.csv';
-COPY comments_by_song (id, song_id, comment_ts, created_ts, comment, user_name, location, followers, pic_url) FROM '/Users/rodrigosanchezgaos/repos/sdc/comments-profile/db/cassandra/comments4.csv';
+COPY comments_by_song (id, song_id, comment_ts, created_ts, comment, user_name, location, followers, pic_url) FROM '/home/ubuntu/comments-service/db/cassandra/comments3.csv';
+COPY comments_by_song (id, song_id, comment_ts, created_ts, comment, user_name, location, followers, pic_url) FROM '/home/ubuntu/comments-service/db/cassandra/comments4.csv';
